4b95358b1cf1f6b702d90709b50874836705e2c3
theunreal <eliraneln@gmail.com>
2021-05-03 17:47:08 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef } from 'react';
+import Webcam from "react-webcam";
+import * as ml5 from 'ml5';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const webcamRef = useRef();
+    const objectDetector = useRef();
+
+    React.useEffect(() => {
+
+        const runCoco = () => {
+            setInterval(() => {
+                detect();
+            }, 2000)
+        };
+
+        webcamRef.current.video.onloadeddata = () => {
+            console.log('Loaded video!');
+            runCoco();
+        };
+        objectDetector.current = ml5.objectDetector('cocossd', {}, modelLoaded);
+    }, []);
+
+    const modelLoaded = () => {
+        console.log('Model loaded!');
+    };
+
+
+    const detect = () => {
+        objectDetector.current.detect(webcamRef.current.video, (err, results) => {
+            console.log(results); // Will output bounding boxes of detected objects
+        });
+    };
+
+    return (
+        <div className="App">
+            <Webcam ref={webcamRef}/>
+        </div>
+    );
 }
 
 export default App;
