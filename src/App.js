0adf9e72fa0d101eccca916494c7f9403447cf72
theunreal <eliraneln@gmail.com>
2021-05-09 10:15:23 +0300

    object detector update

--- src/App.js
+++ src/App.js
@@ -1,45 +1,66 @@
-import './App.css';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from "react";
+import "./App.scss";
 import Webcam from "react-webcam";
-import * as ml5 from 'ml5';
+import * as ml5 from "ml5";
+import glasses from './glasses.png';
 
-
-function App() {
-
-    const webcamRef = useRef();
+const App = () => {
+    const videoRef = useRef();
+    const canvasRef = useRef();
     const objectDetector = useRef();
 
-    React.useEffect(() => {
+    useEffect(() => {
+
+        const ctx = canvasRef.current.getContext('2d');
+        const modelLoaded = () => {
+            setInterval(() => {
+                detect();
+            }, 2000);
+        };
+
+        objectDetector.current = ml5.objectDetector('cocossd', modelLoaded);
 
         const detect = () => {
-            objectDetector.current.detect(webcamRef.current.video, (err, results) => {
-                console.log(results); // Will output bounding boxes of detected objects
+            if (videoRef.current.video.readyState !== 4) {
+                return;
+            }
+            videoRef.current.video.width = 600;
+            videoRef.current.video.height = 420;
+
+            objectDetector.current.detect(videoRef.current.video, (err, results) => {
+                const width = 600;
+                const height = 420;
+                canvasRef.current.width = width;
+                canvasRef.current.height = height;
+                console.log(results);
+                if (results && results.length) {
+                    results.forEach((detection) => {
+                        console.log(detection);
+                        ctx.beginPath();
+                        ctx.fillStyle = "#FF0000";
+                        ctx.rect(detection.x, detection.y, detection.width - 100, detection.height);
+                        ctx.stroke();
+                    });
+
+                }
             });
         };
 
-        const runCoco = () => {
-            setInterval(() => {
-                detect();
-            }, 2000)
-        };
-
-        webcamRef.current.video.onloadeddata = () => {
-            console.log('Loaded video!');
-            runCoco();
-        };
-        objectDetector.current = ml5.objectDetector('cocossd', {}, modelLoaded);
     }, []);
 
-    const modelLoaded = () => {
-        console.log('Model loaded!');
-    };
 
 
     return (
-        <div className="App">
-            <Webcam ref={webcamRef}/>
+        <div className="container">
+            <div className="webcam-video">
+                <Webcam
+                    ref={videoRef}
+                    className="my"
+                />
+                <canvas ref={canvasRef} className="my" />
+            </div>
         </div>
     );
-}
+};
 
 export default App;
